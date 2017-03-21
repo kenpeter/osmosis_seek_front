8d6ec9919cfd7f04b1c1b09d743ab57dbac50efc
kenpeter <figo2476@hotmail.com>
2017-03-21 15:38:26 +1100

    update

---
+++ README.md
@@ -0,0 +1,8 @@
+# osmosis_seek_front
+
+# install
+yarn install
+
+# run
+
+yarn start
---
+++ src/components/Main.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="main">
+    <div class="card">
+      <div class="card-header">
+        Featured
+      </div>
+      <div class="card-block">
+        <h4 class="card-title">Special title treatment</h4>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      jobs: [],
+    };
+  },
+  mounted: () => {
+    this.loadJob();
+  },
+  methods: {
+    loadJob: () => {
+      this.axios.get('http://seekback.shopshop.space/job/list').then((res) => {
+        this.jobs = res.data;
+        console.log(res.data);
+      },
+      (err) => {
+        /* eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
+        console.log(err);
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
