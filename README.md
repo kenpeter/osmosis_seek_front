af556f76e425f9018c6e605e641ef2ba79121dfc
kenpeter <figo2476@hotmail.com>
2017-03-21 16:27:42 +1100

    update

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # osmosis_seek_front
 
+https://medium.com/@bradfmd/vue-vuex-getting-started-f78c03d9f65#.dsf4nf9wr
+
 # install
 yarn install
 
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="main">
-    <div class="card">
+    <div class="card" v-for="job in jobs">
       <div class="card-header">
-        Featured
+
       </div>
       <div class="card-block">
-        <h4 class="card-title">Special title treatment</h4>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <h4 class="card-title">{{ job.title }}</h4>
+        <p class="card-text">Company: {{ job.advertiser }}</p>
+        <p class="card-text">Company: {{ job.description }}</p>
+        <p class="card-text">Desc: {{ job.description }}</p>
+        <p class="card-text">Type: {{ job.employmentType }}</p>
+        <p class="card-text">Posted: {{ job.datePosted }}</p>
+        <a :href="job.url" class="btn btn-primary" target="_blank">Go</a>
       </div>
     </div>
   </div>
@@ -20,20 +24,23 @@ export default {
       jobs: [],
     };
   },
-  mounted: () => {
-    this.loadJob();
-  },
   methods: {
-    loadJob: () => {
+    // ok.... shit.....
+    // have a look at data() {}
+    // basically data() is shorthand
+    loadJob() {
       this.axios.get('http://seekback.shopshop.space/job/list').then((res) => {
         this.jobs = res.data;
-        console.log(res.data);
+        // console.log(res.data);
       },
       (err) => {
         /* eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
         console.log(err);
       });
     },
+  },
+  mounted() {
+    this.loadJob();
   },
 };
 </script>
